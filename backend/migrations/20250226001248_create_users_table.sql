d6e43188a86aab8bfa5a7cdd34f5250e21a17ec1
Diogo Lemos <lemos.diogo105@gmail.com>
2025-03-10 23:12:40 +0000

    Updated migration for rust

--- backend/migrations/20250226001248_create_users_table.sql
+++ backend/migrations/20250226001248_create_users_table.sql
@@ -1,5 +1,5 @@
 -- Add migration script here
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) NOT NULL,
